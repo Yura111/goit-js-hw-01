9cc31a087c96ce7a25c03a1aab4e446cf2523b44
Yura111 <yur8594@gmail.com>
2020-07-06 22:19:09 +0300

    06.07/22.18

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,11 @@
-let message;
-do {
-    message = prompt ('ADMIN_PASSWORD')  
-    // console.log('Добро пожаловать !')
-if(message === null) {
-console.log('Отменино пользователем !')
-break;
-}
-message = Number(message)}
-while(message !== 5)
-console.log('Добро пожаловать !')
+// let message;
+// do {
+//     message = prompt ('ADMIN_PASSWORD')  
+//     // console.log('Добро пожаловать !')
+// if(message === null) {
+// console.log('Отменино пользователем !')
+// break;
+// }
+// message = Number(message)}
+// while(message !== 5)
+// console.log('Добро пожаловать !')
