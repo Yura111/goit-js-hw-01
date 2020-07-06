467b09178c31e85422880a6bbb9aff97ee796fe5
Yura111 <yur8594@gmail.com>
2020-07-06 16:49:07 +0300

    06.07/16.49

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,6 @@
 const total = 40;
 const ordered = 50;
-// const totalOrdered = total > ordered;
-// const totalOrderedNext = total < ordered;
-
-if(total >= ordered) {
-    console.log('Заказ оформлен, с вами свяжется менеджер')
+if (total >= ordered) {
+  console.log("Заказ оформлен, с вами свяжется менеджер");
 }
-console.log('На складе недостаточно товаров!')
+console.log("На складе недостаточно товаров!");
