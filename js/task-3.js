0aec5df3a009ba87ea1296d125d90492973b6575
Yura111 <yur8594@gmail.com>
2020-07-07 18:03:56 +0300

    07.07/18.03

--- js/task-3.js
+++ js/task-3.js
@@ -1,24 +1,12 @@
+
+// const ADMIN_PASSWORD = "abc";
 // let message;
-// do {
-//     message = prompt ('ADMIN_PASSWORD')  
-//     // console.log('Добро пожаловать !')
-// if(message === null) {
-// console.log('Отменино пользователем !')
-// break;
+// let input = prompt("Добро пожаловать!");
+// if (input === null) {
+//   message = "Отменено пользователем";
+// } else if (input === ADMIN_PASSWORD) {
+//   message = "Добропожаловать!";
+// } else {
+//   message = "Доступ запрещен, неверный пароль!";
 // }
-// message = Number(message)}
-// while(message !== 5)
-// console.log('Добро пожаловать !')
-const ADMIN_PASSWORD = 'abc';
-let message;
-let input = prompt('Добро пожаловать!');
-if (input===null){
-    message = 'Отменено пользователем';
-    } else if (input === ADMIN_PASSWORD){
-    message = 'Добропожаловать!';
-    } else {
-        message = 'Доступ запрещен, неверный пароль!';
-    }
-    alert(message);
-
-
+// alert(message);
