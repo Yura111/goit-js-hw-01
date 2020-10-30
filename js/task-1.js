c64a36f3d81bff3b1c0b9b8124d4598c59e16762
Yura111 <yur8594@gmail.com>
2020-10-30 10:59:18 +0200

    30.10.2020

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,13 @@
-let name = "название:Генератор защитного поля";
-let price = "цена: 1000";
-console.log(name);
-console.log(price);
-let nameNext = ` "Генератор защитного поля",`;
-let priceNext = `цена за штуку 1000 кредитов`;
-let priceNextTwo = `цена за штуку 2000 кредитов`;
-let message = `Выбран ${nameNext} ${priceNext}`;
-let messageTwo = `Выбран ${nameNext} ${priceNextTwo}`;
-console.log(message);
-console.log(messageTwo);
+// let name = "название:Генератор защитного поля";
+// let price = "цена: 1000";
+// console.log(name);
+// console.log(price);
+// let nameNext = ` "Генератор защитного поля",`;
+// let priceNext = `цена за штуку 1000 кредитов`;
+// let priceNextTwo = `цена за штуку 2000 кредитов`;
+// let message = `Выбран ${nameNext} ${priceNext}`;
+// let messageTwo = `Выбран ${nameNext} ${priceNextTwo}`;
+// console.log(message);
+// console.log(messageTwo);
+
+
--- js/task-6.js
+++ js/task-6.js
@@ -1,16 +1,16 @@
-let total = 0;
-while (true) {
-  let input = prompt("введи число");
-  if (input === null) {
-    console.log("Отменено пользователем!");
-    break;
-  }
-  input = Number(input);
-  const notNumber = Number.isNaN(input);
-  if (notNumber) {
-    alert("Было введено не число, попробуйте еще раз");
-    continue;
-  }
-  total += input;
-}
-alert(`Общая сума ${total}`);
+// let total = 0;
+// while (true) {
+//   let input = prompt("введи число");
+//   if (input === null) {
+//     console.log("Отменено пользователем!");
+//     break;
+//   }
+//   input = Number(input);
+//   const notNumber = Number.isNaN(input);
+//   if (notNumber) {
+//     alert("Было введено не число, попробуйте еще раз");
+//     continue;
+//   }
+//   total += input;
+// }
+// alert(`Общая сума ${total}`);
